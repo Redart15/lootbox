b37f49f7603f62d9c5c6d3836b3f473c234bf199
Artem Betz <arbet100@uni-duesseldorf.de>
2023-07-20 11:04:35 +0200

    changed the chance to float

--- lootbox.py
+++ lootbox.py
@@ -127,6 +127,7 @@ def remove_conditions(toadd):
 
 def remove_functions(toadd):
     if "functions" in toadd:
+        print(entry)
         del toadd["functions"]
 
 def remove_2ndConditions(toadd):
@@ -190,7 +191,7 @@ def randomInt(start,stop):
         random_index = random.randint(start, stop-1)
     return random_index
 
-def write_2zipstream(version, datapack_name, datapack_description, filepath_list, loottables, min_value, max_value, frequenzy, zipbytes):
+def write_2zipstream(version, datapack_name, datapack_description, filepath_list, loottables, min_value, max_value, chance, zipbytes):
     print("Beginning writting...")
     prefix_name = "lootbox_{}.json"
     prefix_path = 'data/minecraft/'
@@ -198,7 +199,7 @@ def write_2zipstream(version, datapack_name, datapack_description, filepath_list
 
     zipstream = zipfile.ZipFile(zipbytes, 'w', zipfile.ZIP_DEFLATED, False)
     zipstream_lootboxes(min_value, max_value, combined_table_path, prefix_name, loottables, zipstream)
-    zipstream_editedItems(filepath_list, prefix_path, len(loottables), frequenzy, zipstream)
+    zipstream_editedItems(filepath_list, prefix_path, len(loottables), chance, zipstream)
     zipstream_metadata(version, datapack_name, datapack_description, zipstream)
     zipstream.close()
 
@@ -210,31 +211,49 @@ def zipstream_lootboxes(min_value, max_value, combined_table_path, prefix_name, 
         source = os.path.join(combined_table_path,name)
         zip.writestr(source, json.dumps(output_data, indent=4))
 
-def zipstream_editedItems(filepath_list, prefix_path, size_loottable, frequenzy, zip):
+def zipstream_editedItems(filepath_list, prefix_path, size_loottable, chance, zip):
     index = randomInt(0,size_loottable-1)
     print("Writting and adjusting loottables...")
     for file in filepath_list:
         index = (index + 1) % size_loottable
-        data = add_lootbox_2items(file, index, frequenzy)
+        data = add_lootbox_2items(file, index, chance)
         zip.writestr(os.path.join(prefix_path, file), json.dumps(data,indent=4))
 
-def add_lootbox_2items(file, index, frequenzy): # does not use loottable
+def add_lootbox_2items(file, index, chance): # does not use loottable
     data = load_json(file)
     if not "pools" in data:
         return data
     list_entries = data["pools"]          
-    list_entries.append(make_lootboxPool(index, len(list_entries), frequenzy))
+    list_entries.append(make_lootboxPool(index, len(list_entries), chance))
     return data
 
 # leaves and chest have multiple pools, modifing the pools results in more loot than expected
 # comprimise: the original block is not consumed 
-def make_lootboxPool(index, size, frequenzy):
+def make_lootboxPool(index, size, chance):
+    # fraction = Fraction(chance/100)
+    # total_weight = fraction.denominator
+    # loot_weight = fraction.numerator
+    # empty_weight = total_weight - loot_weight
+    loot_weight = convert_toInt(chance)
+    factor = 100.0/chance
+    total = int(round(loot_weight * factor,0))
+    empty_weight = total - loot_weight
     lootbox_path = "minecraft:lootboxes/lootbox_{}".format(index)
-    lootbox_entry = {"type":'minecraft:loot_table',"name":lootbox_path,"weight":1}
-    empty_entry = {"type":'minecraft:empty',"weight": size * frequenzy}
+    lootbox_entry = {"type":'minecraft:loot_table',"name":lootbox_path,"weight":loot_weight}
+    empty_entry = {"type":'minecraft:empty',"weight": size * empty_weight}
     # its rolls not rools u fucking idiot, and its a float not an int !
     new_pool = {"rolls":1.0,"bonus_rolls":0.0,"entries":[lootbox_entry,empty_entry]}
     return new_pool
+
+def convert_toInt(chance):
+    string = str(chance)
+    if '.' in string:
+        index = string.index('.')
+        digits_after_dot = len(string) - index - 1
+    else:
+        digits_after_dot = 0
+
+    return int(chance * math.pow(10,digits_after_dot))
 
 def zipstream_metadata(version, datapack_name, datapack_description, zipstream):
     print("Writting metadata....")
@@ -272,12 +291,14 @@ def main():
             config_data = yaml.load(config_file, Loader=yaml.FullLoader)
         version = read_config(config_data,"version",16,int)
         box_count = read_config(config_data,"box_count",50,int)
-        frequenzy = read_config(config_data,"frequenzy",3,int)
+        chance = read_config(config_data,"chance",25.0,float)
         min_value = read_config(config_data,"min_value",1,int)
         max_value = read_config(config_data,"max_value",5,int)
         isUnit = read_config(config_data,"isUnit",True,bool)
 
-
+        if chance > 1_000 or chance < 0.001:
+            chance = 25.0
+            print("Using default value {} for {}".format(25.0,"chance"))
 
     if len(sys.argv) >= 2:
         try:
@@ -291,7 +312,7 @@ def main():
         seed = int.from_bytes(random_data, byteorder="big")  
 
     random.seed(seed)
-    datapack_name = 'lootbox_{}'.format(seed)
+    datapack_name = 'lootbox'.format(seed)
     
 
     print('Generating datapack...')
@@ -302,7 +323,7 @@ def main():
     zipbytes = io.BytesIO()
     datapack_filename = '{}.zip'.format(datapack_name)
     datapack_description = 'Lootboxes, Box Count:{}, Seed:{}'.format(box_count,seed)
-    write_2zipstream(version, datapack_name, datapack_description, filepath_list, loottables, min_value, max_value, frequenzy, zipbytes)
+    write_2zipstream(version, datapack_name, datapack_description, filepath_list, loottables, min_value, max_value, chance, zipbytes)
 
     print('Zipping files...')
     with open(datapack_filename, 'wb') as file:
@@ -314,29 +335,30 @@ def main():
 config_context = """
 # 1.20 is Version 16, every major release add 1 to version
 # be warned this script may not be compatable with older versions
-# type Integer
+# type integer
 version: 16 
 
 # determins how many different lootboxes there will be (1-100 recommended)
-# type Integer
+# type integer
 box_count: 50 
 
 # sets how many rolls are made on the lootbox tables itself
-# type Integer
+# type integer
 min_value: 1
 max_value: 5
 
 # odd of rolling the table, 50 means 50%
-# type Integer
-chance: 25 
+# type dezimal
+chance: 25.0
 
 # some table are made up of a lot of subtables, keep all these table together when distrubitung into lootboxes
-# type Boolean
+# type boolean
 isUnit: True
 """
 
 if __name__ == '__main__':
-    excluded_dir = ['combined',"combiined"]
+    excluded_dir = []
+    # exclude_dir = ["archaeology", "chests", "entities", "gameplay"]
     main()
 
 
